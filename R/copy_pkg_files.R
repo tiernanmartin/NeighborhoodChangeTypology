92d062e43f0bd234d625732aeb26bd467b173420
Tiernan Martin <tiernan.martin@gmail.com>
2018-07-31 13:03:04 -0700

    remove plans/ from the copy_pkg_file() function

--- R/copy_pkg_files.R
+++ R/copy_pkg_files.R
@@ -5,14 +5,12 @@
 #'   to write the folders containing the workflow's files.
 #' @param overwrite_doc Logical, whether to overwrite the `doc/` directory (default = `TRUE`)
 #' @param overwrite_extdata Logical, whether to overwrite the `extdata/` directory (default = `TRUE`)
-#' @param overwrite_plans Logical, whether to overwrite the `plans/` directory (default = `TRUE`)
 #' @note This function will copy the following directories (and their subdirectories and files) into
 #'   the user's working directory (or the directory supplied to the \code{to} parameter): \cr
 #'   `
 #'    . \cr
 #'    ├── doc/ \cr
-#'    ├── extdata/ \cr
-#'    └── plans/ \cr
+#'    └── extdata/ \cr
 #'   `
 #'
 #' @return `NULL`
@@ -21,16 +19,15 @@
 #' @export
 copy_pkg_files <- function(to = getwd(),
                            overwrite_doc = TRUE,
-                           overwrite_extdata = TRUE,
-                           overwrite_plans = TRUE){
+                           overwrite_extdata = TRUE){
 
   dir_list <- map(
-    list("doc", "extdata", "plans"),
+    list("doc", "extdata"),
     system.file,
     package = "drakepkg",
     mustWork = TRUE)
 
-  overwrite_list <- list(overwrite_doc, overwrite_extdata, overwrite_plans)
+  overwrite_list <- list(overwrite_doc, overwrite_extdata)
 
   walk2(dir_list,
         overwrite_list,
