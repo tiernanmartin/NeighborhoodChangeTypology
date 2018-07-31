8444ebbb5f7190d9a67c747567c4cef6f76f3b62
Tiernan Martin <tiernan.martin@gmail.com>
2018-07-31 09:34:03 -0700

    create data_external_datasets_list (placeholder)

---
+++ R/data.R
@@ -0,0 +1,23 @@
+#' @title External Data List
+#' @description This is a list of datasets that are included in the package workflow but
+#'   are not stored in the \code{data/} directory.
+#'
+#'   The primary reason that a dataset is included in this list is that it exceeds Github's
+#'   50 MB file size limit (see \href{https://help.github.com/articles/conditions-for-large-files/}{here} for details).
+#'
+#' @docType data
+#' @format The data itself is simply a \code{NULL} value.
+#'
+#'  Its purpose is to provide some documentation of the datasets that are not directly
+#'  included in the package.
+#'
+#'  The following external datasets are accessed by workflows in this package:
+#'
+#' \describe{
+#'
+#'   \item{`• item_one`:}{Description(\href{www.url.com}{link})}
+#'
+#'   }
+#'
+#' @seealso \code{\link{data_exteral_datasets_list}}
+"data_exteral_datasets_list"
---
+++ inst/extdata/R/make-data-exteral-datasets-list.R
@@ -0,0 +1,3 @@
+data_exteral_datasets_list <- NULL
+
+usethis::use_data(data_exteral_datasets_list)
