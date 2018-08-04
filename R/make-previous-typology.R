087b8a73cdc158fc5b68d808974e54b92d9d3b74
Tiernan Martin <tiernan.martin@gmail.com>
2018-08-04 15:12:07 -0700

    add a function to read the typology data from the 2016 analysis

---
+++ R/make-previous-typology.R
@@ -0,0 +1,7 @@
+#' @title Make Previous Typology
+#' @description This is a temporary function.
+#' @return a MULTIPOLYGON simple feature (class =  `sf`)
+#' @export
+make_previous_typology <- function(){
+  readRDS("extdata/typology.rds")
+}
