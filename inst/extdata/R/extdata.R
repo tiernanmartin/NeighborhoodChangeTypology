74373d529bc0233bb21280228e3ad7ab03505698
Tiernan Martin <tiernan.martin@gmail.com>
2018-08-08 09:47:06 -0700

    add the script for downloading King County parcel boundaries from their source

--- inst/extdata/R/extdata.R
+++ inst/extdata/R/extdata.R
@@ -4,3 +4,32 @@
 exteral_datasets_list <- NULL
 
 usethis::use_data(exteral_datasets_list)
+
+
+
+# KING COUNTY PARCEL BOUNDARIES -------------------------------------------
+
+library(osfr)
+library(tidyverse)
+library(sf)
+
+
+url <- "ftp://ftp.kingcounty.gov/gis-web/GISData/parcel_SHP.zip"
+
+tmp <- tempfile()
+
+download.file(url, tmp)
+
+unzip( tmp,exdir = "extdata/parcel_SHP")
+
+parcel <- read_sf("extdata/parcel_SHP/parcel")
+
+
+parcel_fp <- "extdata/kc-parcels-spatial.gpkg"
+
+st_write(parcel, dsn = parcel_fp, driver = "GPKG")
+
+
+login() # this won'r work for anyone except Tiernan Martin
+
+osfr::upload_files(id = "sj7n9", path = parcel_fp)
