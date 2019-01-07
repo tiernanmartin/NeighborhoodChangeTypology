78fe524b5579d4260de3a12de6b1feda27cd54c4
Tiernan Martin <tiernan.martin@gmail.com>
2019-01-06 23:19:27 -0800

    add `indicators` to `indicator_plan`

---
+++ R/make-indicators.R
@@ -0,0 +1,21 @@
+#' @title Make All Indicators
+#' @description Desc
+#' @param indicators_cnt_pct desc
+#' @param indicators_median desc
+#' @param sample_size_metadata desc
+#' @return a `tibble`
+#' @export
+make_indicators <- function(indicators_cnt_pct,
+                                 indicators_median,
+                                 sample_size_metadata){
+
+  inds_all <- list(indicators_cnt_pct,
+                                 indicators_median) %>%
+    purrr::map_dfr(c) %>%
+    dplyr::left_join(sample_size_metadata, by = c("SOURCE", "INDICATOR", "VARIABLE", "MEASURE_TYPE"))
+
+  indicators <- inds_all
+
+  return(indicators)
+
+}
