aaa64a22b536493426405767f70c0bfd6284ea4d
Tiernan Martin <tiernan.martin@gmail.com>
2019-02-05 14:59:52 -0800

    add a check to get_osf_version()

--- R/get-osf-version.R
+++ R/get-osf-version.R
@@ -12,6 +12,8 @@ get_osf_version <- function(id = NULL, filename = NULL){
 
   files_info <- osfr:::get_files_info(id, private = FALSE)
 
+  if(!filename %in% files_info$name){stop(glue::glue("The string passed to the `filename` argument ({filename}) doesn't match any of the file names in this OSF folder.\nVisit the OSF folder and double-check the file name of the file you are trying to update."))}
+
   version <- files_info[files_info$name %in% filename,"version"][[1]]
 
   return(version)
