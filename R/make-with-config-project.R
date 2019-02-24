d1851de7be804ef93b0d7394bfb7a54e828d86ab
Tiernan Martin <tiernan.martin@gmail.com>
2019-02-24 11:18:01 -0800

    turn off garbage_collection in make_with_config()

--- R/make-with-config-project.R
+++ R/make-with-config-project.R
@@ -1,16 +1,17 @@
 #' @title Make a (Congifured) Drake Plan
 #' @description Make a \code{drake} plan using the project's speficic configuration setup.
 #' @param plan drake plan
+#' @param .garbage_collection logical (default = FALSE), whether to run \code{gc()} on the plan
 #' @param .beep logical (default = TRUE), whether to play the "ping" sound after the plan is made
 #' @param .beep_on_error logical (default = TRUE), whether to play the "facebook" sound if the plan returns an error
 #' @return The master internal configuration list, mostly containing arguments to make() and important objects constructed along the way. See drake_config() for more details.
 #' @note See the \code{drake} manual for details: \url{https://ropenscilabs.github.io/drake-manual/hpc.html#on-your-local-machine-1}.
 #' @export
-make_with_config_project <- function(plan,.beep = TRUE,.beep_on_error = TRUE){
+make_with_config_project <- function(plan, .garbage_collection = FALSE, .beep = TRUE,.beep_on_error = TRUE){
 
   # future::plan(future::multiprocess)
 
-  make_plan_expr <- function(){drake::make(plan = plan, garbage_collection = TRUE)}
+  make_plan_expr <- function(){drake::make(plan = plan, garbage_collection = .garbage_collection)}
 
   # beep on either success or error
   if(.beep & .beep_on_error){
