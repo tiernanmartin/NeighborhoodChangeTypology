f9953957139a00b8146dda46c4051db0d85ecc54
Tiernan Martin <tiernan.martin@gmail.com>
2019-02-28 15:31:15 -0800

    show model_pdx16 in show_typology_map()

--- R/show-typology-map.R
+++ R/show-typology-map.R
@@ -1,17 +1,9 @@
 #' @title SHow Typology Map
 #' @description This is a temporary function.
-#' @param typology typology
-#' @param white_center_place white center boundary
+#' @param model_pdx16 desc
 #' @return a MULTIPOLYGON simple feature (class =  `sf`)
 #' @export
-show_typology_map <- function(typology, white_center_place){
-
-  my_pal <- c(
-    RColorBrewer::brewer.pal(n = 9,'YlOrRd')[c(3,5,6)],
-    RColorBrewer::brewer.pal(n = 9,'RdPu')[c(6)],
-    RColorBrewer::brewer.pal(n = 9,'YlGnBu')[c(6,7)]
-  )
-
+show_typology_map <- function(model_pdx16){
 
   wc_tr <- c("53033026600",
              "53033026700",
@@ -20,27 +12,43 @@ show_typology_map <- function(typology, white_center_place){
              "53033026802",
              "53033027000")
 
-  wc <- typology %>% dplyr::filter(GEOID %in% wc_tr) %>% sf::st_transform(4326)
+  model_pdx16_sf <- model_pdx16 %>%
+  dplyr::mutate(PDX18_TYPE_FCT = factor(PDX18_TYPE,
+                                        levels = c("Susceptible",
+                                                   "Early Type 1",
+                                                   "Early Type 2",
+                                                   "Dynamic",
+                                                   "Late",
+                                                   "Continued Loss"
+                                        ))) %>%
+  dplyr::left_join(census_tracts_2016_trimmed,
+                   by = c("GEOGRAPHY_ID",
+                          "GEOGRAPHY_ID_TYPE",
+                          "GEOGRAPHY_NAME",
+                          "GEOGRAPHY_TYPE")) %>%
+  sf::st_sf() %>%
+  sf::st_collection_extract("POLYGON")
 
-  pal <- leaflet::colorFactor(my_pal,levels = levels(typology$TYPO_FCT), ordered = TRUE,na.color = 'transparent')
+my_pal <- c(
+  RColorBrewer::brewer.pal(n = 9,'YlOrRd')[c(3,5,6)],
+  RColorBrewer::brewer.pal(n = 9,'RdPu')[c(6)],
+  RColorBrewer::brewer.pal(n = 9,'YlGnBu')[c(6,7)]
+)
 
-  vals <- levels(ordered(typology$TYPO_FCT))
+pal <- leaflet::colorFactor(my_pal,levels = levels(model_pdx16_sf$PDX18_TYPE_FCT), ordered = TRUE,na.color = 'transparent')
 
-  typology %>%
-    sf::st_transform(4326) %>%
-    dplyr::mutate(POPUP= paste0(GEOID,": ",TYPO_CHR)) %>%
-    leaflet::leaflet() %>%
-    leaflet::addProviderTiles(provider = leaflet::providers$OpenStreetMap) %>%
-    leaflet::addPolygons(fillColor = ~pal(TYPO_FCT), fillOpacity = .75, smoothFactor = 0,
-                         color = 'white', opacity = .85, weight = .5,
-                         popup = ~POPUP) %>%
-    leaflet::addLegend(title = '',position = 'topright',pal = pal, values = ~TYPO_FCT, opacity = .75) %>%
-    leaflet::addPolygons(data = wc,
-                         fillOpacity = 0, smoothFactor = 0,
-                         color = 'blue', opacity = .75, weight = 1.5) %>%
-    leaflet::addPolygons(data = sf::st_transform(white_center_place, 4326),
-                         fillOpacity = 0, smoothFactor = 0,
-                         color = 'black', opacity = .95, weight = 2)
+vals <- levels(ordered(model_pdx16_sf$PDX18_TYPE_FCT))
+
+model_pdx16_sf %>%
+  sf::st_transform(4326) %>%
+  dplyr::mutate(POPUP= paste0(GEOGRAPHY_NAME,": ",GEOGRAPHY_ID)) %>%
+  leaflet::leaflet() %>%
+  leaflet::addProviderTiles(provider = leaflet::providers$OpenStreetMap) %>%
+  leaflet::addPolygons(fillColor = ~pal(PDX18_TYPE_FCT), fillOpacity = .75, smoothFactor = 0,
+                       color = 'white', opacity = .85, weight = .5,
+                       popup = ~POPUP) %>%
+  leaflet::addLegend(title = '',position = 'topright',pal = pal, values = ~PDX18_TYPE_FCT, opacity = .75)
+
 }
 
 
