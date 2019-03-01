dcbc64644d6500c3cb6c65abe7e1f4d63144485f
Tiernan Martin <tiernan.martin@gmail.com>
2019-02-28 23:48:36 -0800

    refine show_typology_map()

--- R/show-typology-map.R
+++ R/show-typology-map.R
@@ -1,9 +1,30 @@
 #' @title SHow Typology Map
 #' @description This is a temporary function.
-#' @param model_pdx16 desc
+#' @param model_all desc
 #' @return a MULTIPOLYGON simple feature (class =  `sf`)
 #' @export
-show_typology_map <- function(model_pdx16){
+show_typology_map <- function(model_all){
+
+  # assign one of the model objects to model_df
+
+  model_colnames <- c("PDX18_TYPE",       # 1
+                      "COO16_TYPE",       # 2
+                      "COO18_TYPE",       # 3
+                      "COOREV18_MF_TYPE", # 4
+                      "COOREV18_SF_TYPE") # 5
+
+
+  model_ready <- model_all %>%
+    dplyr::select(dplyr::starts_with("GEOGRAPHY"),
+                  tidyselect::vars_select(names(.),model_colnames[4])) %>%
+    dplyr::mutate(COOREV18_MF_TYPE_FCT = factor(COOREV18_MF_TYPE,
+                                        levels = c("Susceptible",
+                                                   "Early Type 1",
+                                                   "Early Type 2",
+                                                   "Dynamic",
+                                                   "Late",
+                                                   "Continued Loss"
+                                        )))
 
   wc_tr <- c("53033026600",
              "53033026700",
@@ -12,42 +33,25 @@ show_typology_map <- function(model_pdx16){
              "53033026802",
              "53033027000")
 
-  model_pdx16_sf <- model_pdx16 %>%
-  dplyr::mutate(PDX18_TYPE_FCT = factor(PDX18_TYPE,
-                                        levels = c("Susceptible",
-                                                   "Early Type 1",
-                                                   "Early Type 2",
-                                                   "Dynamic",
-                                                   "Late",
-                                                   "Continued Loss"
-                                        ))) %>%
-  dplyr::left_join(census_tracts_2016_trimmed,
-                   by = c("GEOGRAPHY_ID",
-                          "GEOGRAPHY_ID_TYPE",
-                          "GEOGRAPHY_NAME",
-                          "GEOGRAPHY_TYPE")) %>%
-  sf::st_sf() %>%
-  sf::st_collection_extract("POLYGON")
-
 my_pal <- c(
   RColorBrewer::brewer.pal(n = 9,'YlOrRd')[c(3,5,6)],
   RColorBrewer::brewer.pal(n = 9,'RdPu')[c(6)],
   RColorBrewer::brewer.pal(n = 9,'YlGnBu')[c(6,7)]
 )
 
-pal <- leaflet::colorFactor(my_pal,levels = levels(model_pdx16_sf$PDX18_TYPE_FCT), ordered = TRUE,na.color = 'transparent')
+pal <- leaflet::colorFactor(my_pal,levels = levels(model_ready$COOREV18_MF_TYPE_FCT), ordered = TRUE,na.color = 'transparent')
 
-vals <- levels(ordered(model_pdx16_sf$PDX18_TYPE_FCT))
+vals <- levels(ordered(model_ready$COOREV18_MF_TYPE_FCT))
 
-model_pdx16_sf %>%
+model_ready %>%
   sf::st_transform(4326) %>%
   dplyr::mutate(POPUP= paste0(GEOGRAPHY_NAME,": ",GEOGRAPHY_ID)) %>%
   leaflet::leaflet() %>%
   leaflet::addProviderTiles(provider = leaflet::providers$OpenStreetMap) %>%
-  leaflet::addPolygons(fillColor = ~pal(PDX18_TYPE_FCT), fillOpacity = .75, smoothFactor = 0,
+  leaflet::addPolygons(fillColor = ~pal(COOREV18_MF_TYPE_FCT), fillOpacity = .75, smoothFactor = 0,
                        color = 'white', opacity = .85, weight = .5,
                        popup = ~POPUP) %>%
-  leaflet::addLegend(title = '',position = 'topright',pal = pal, values = ~PDX18_TYPE_FCT, opacity = .75)
+  leaflet::addLegend(title = '',position = 'topright',pal = pal, values = ~COOREV18_MF_TYPE_FCT, opacity = .75)
 
 }
 
