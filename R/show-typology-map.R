65e425ffbcbb5a57a05d4f45f4c47dca7c8d2678
Tiernan Martin <tiernan.martin@gmail.com>
2019-03-02 13:34:39 -0800

    improve the layering of the leaflet map

--- R/show-typology-map.R
+++ R/show-typology-map.R
@@ -1,9 +1,10 @@
 #' @title SHow Typology Map
 #' @description This is a temporary function.
 #' @param model_all desc
+#' @param kc_boundary desc
 #' @return a MULTIPOLYGON simple feature (class =  `sf`)
 #' @export
-show_typology_map <- function(model_all){
+show_typology_map <- function(model_all, kc_boundary){
 
   # assign one of the model objects to model_df
 
@@ -13,11 +14,14 @@ show_typology_map <- function(model_all){
                       "COOREV18_MF_TYPE", # 4
                       "COOREV18_SF_TYPE") # 5
 
+  kc_boundary <- sf::st_transform(kc_boundary, 4326)
+
+  model_all <- sf::st_transform(model_all, 4326)
 
   model_ready <- model_all %>%
     dplyr::select(dplyr::starts_with("GEOGRAPHY"),
-                  tidyselect::vars_select(names(.),model_colnames[4])) %>%
-    dplyr::mutate(COOREV18_MF_TYPE_FCT = factor(COOREV18_MF_TYPE,
+                  tidyselect::vars_select(names(.),model_colnames[5])) %>%
+    dplyr::mutate(COOREV18_SF_TYPE_FCT = factor(COOREV18_SF_TYPE,
                                         levels = c("Susceptible",
                                                    "Early Type 1",
                                                    "Early Type 2",
@@ -39,19 +43,41 @@ my_pal <- c(
   RColorBrewer::brewer.pal(n = 9,'YlGnBu')[c(6,7)]
 )
 
-pal <- leaflet::colorFactor(my_pal,levels = levels(model_ready$COOREV18_MF_TYPE_FCT), ordered = TRUE,na.color = 'transparent')
+my_pal_hex <- c("#ffff00",
+                "#ffc800",
+                "#ff9600",
+                "#a30382",
+                "#0794d0",
+                "#1b5f8f")
 
-vals <- levels(ordered(model_ready$COOREV18_MF_TYPE_FCT))
+
+pal <- leaflet::colorFactor(my_pal_hex,levels = levels(model_ready$COOREV18_SF_TYPE_FCT), ordered = TRUE,na.color = 'transparent')
+
+vals <- levels(ordered(model_ready$COOREV18_SF_TYPE_FCT))
 
 model_ready %>%
   sf::st_transform(4326) %>%
   dplyr::mutate(POPUP= paste0(GEOGRAPHY_NAME,": ",GEOGRAPHY_ID)) %>%
   leaflet::leaflet() %>%
-  leaflet::addProviderTiles(provider = leaflet::providers$OpenStreetMap) %>%
-  leaflet::addPolygons(fillColor = ~pal(COOREV18_MF_TYPE_FCT), fillOpacity = .75, smoothFactor = 0,
+  leaflet::addMapPane("background_map", zIndex = 410) %>%
+  leaflet::addMapPane("polygons", zIndex = 420) %>%
+  leaflet::addMapPane("lines", zIndex = 430) %>%
+  leaflet::addMapPane("labels", zIndex = 440) %>%
+  leaflet::addProviderTiles(provider = leaflet::providers$Stamen.TonerLite,
+                            options = pathOptions(pane = "background_map")) %>%
+  leaflet::addPolygons(fillColor = ~pal(COOREV18_SF_TYPE_FCT), fillOpacity = 1, smoothFactor = 0,
                        color = 'white', opacity = .85, weight = .5,
-                       popup = ~POPUP) %>%
-  leaflet::addLegend(title = '',position = 'topright',pal = pal, values = ~COOREV18_MF_TYPE_FCT, opacity = .75)
+                       popup = ~POPUP,
+                       options = pathOptions(pane = "polygons")) %>%
+  leaflet::addPolygons(data = kc_boundary,
+                       fillOpacity = 0, smoothFactor = 0,
+                       color = 'black', opacity = .85, weight = 5,
+                       options = pathOptions(pane = "polygons")) %>%
+    leaflet::addProviderTiles(provider = leaflet::providers$Stamen.TonerLines,
+                            options = pathOptions(pane = "lines")) %>%
+  leaflet::addProviderTiles(provider = leaflet::providers$Stamen.TonerLabels,
+                            options = pathOptions(pane = "labels"))
+  # leaflet::addLegend(title = '',position = 'topright',pal = pal, values = ~COO16_TYPE_FCT, opacity = 1)
 
 }
 
